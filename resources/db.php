5f3b4cf444f7a1fa99e3a48f1315cf6d8cd59c3d
shahadh <shahadh.hamza@gmail.com>
2021-05-25 20:49:17 +0530

    php code for get db connection

---
+++ resources/db.php
@@ -0,0 +1,33 @@
+<?php
+require ('config.php');
+class DbConnection
+{
+
+    private $DB_SERVER = DB_HOST;
+    private $DB_USER = DB_USER;
+    private $DB_PASSWORD = DB_PASSWORD;
+    private $DB_NAME = DB_NAME;
+    public $connection;
+
+
+    private function dbConnect()
+    {
+
+        $this->connection = null;
+        try {
+            $this->connection = new PDO("mysql:host=localhost;dbname=" . $this->DB_NAME, $this->DB_USER, $this->DB_PASSWORD);
+            $this->connection->exec("set names utf8");
+
+        } catch (PDOException $exception) {
+            echo "Db Error:" . $exception;
+        }
+        return $this->connection;
+    }
+
+    public function getConnection(){
+        return $this->dbConnect();
+    }
+
+}
+
+?>
