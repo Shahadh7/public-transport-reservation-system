c04b7b18c3e6451b6e5d4324a2e8ed856cf7b90d
shahadh <shahadh.hamza@gmail.com>
2021-05-25 20:10:12 +0530

    initial commit

---
+++ templates/footer.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" href="css/style.css">
+<div class="footer">
+    <p>Public transport reservation system © 2021</p>
+</div>
---
+++ templates/header.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" href="css/style.css">
+<ul>
+    <li id="logo"><p>Public Transport Reservation System</p></li>
+    <li><a href="#">About Us</a></li>
+    <li><a href="#">Login</a></li>
+    <li><a href="#">Register</a></li>
+    <li><a href="#">Home</a></li>
+</ul>
