b5abf7c45299fb57a795659f4d5949e364aeb2b8
shahadh <shahadh.hamza@gmail.com>
2021-06-01 00:23:51 +0530

    changes

--- views/signup.php
+++ views/signup.php
@@ -5,6 +5,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signup</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body class="bg5">
+    <section class="header-section">
+        <?php include '../templates/header.php' ?>
+    </section>    
     <section class="content-section">
         <div id="signup-div">
             <div class="signup-form">
